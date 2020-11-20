da599e47f7b5868a402b8e9841c5cf7b7ac4e378
MAss330 <50790617+MAss330@users.noreply.github.com>
2020-11-20 20:44:18 +0800

    Create 1122_2.py
    组的相对排序——计数法

---
+++ 1122_2.py
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        // 计数的方法
+        int[] count = new int[1001];
+        int[] ans = new int[arr1.length];
+        for (int val : arr1){
+            count[val]++; 
+        }
+        int index = 0;
+        for (int num : arr2){
+            while (count[num] > 0){
+                count[num]--;
+                ans[index++] = num;
+            }
+        }
+
+        for (int i = 0; i < count.length; i++){
+            while (count[i] > 0){
+                count[i]--;
+                ans[index++] = i;
+            }
+        }
+
+        return ans;
+
+
+
+
+        
+
+    }
+}
