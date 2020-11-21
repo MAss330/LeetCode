51feef5bee6997dfcfc0ea844ce079f5dd451454
MAss330 <50790617+MAss330@users.noreply.github.com>
2020-11-21 14:10:18 +0800

    Create 21_2.py
    迭代法

---
+++ 21_2.py
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // 迭代法
+        ListNode dummpy = new ListNode(0);
+        ListNode pre = dummpy;
+        while (l1 != null && l2 != null){
+            if (l1.val <= l2.val){
+                pre.next = l1;
+                l1 = l1.next;
+            }else{
+                pre.next = l2;
+                l2 = l2.next;
+            }
+            pre = pre.next;
+        }
+
+        pre.next = l1 == null ? l2 : l1;
+
+        return dummpy.next;
+        
+    }
+}
